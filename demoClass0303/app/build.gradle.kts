cf90a6874c64cf04879f12f58e276da6648dfa82
sophiahua03 <tien.hnt.63cntt@ntu.edu.vn>
2025-03-03 16:52:57 +0700

    DemoListener (Class) 03/03/25

---
+++ demoClass0303/app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "tien63131427.democlass0303"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "tien63131427.democlass0303"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ demoClass0303/app/src/main/java/tien63131427/democlass0303/MainActivity.java
@@ -0,0 +1,43 @@
+package tien63131427.democlass0303;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnSayHi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnSayHi = findViewById(R.id.btnSayHello);
+        btnSayHi.setOnClickListener(boLangNghe_XinChao);
+    }
+
+    // 2.1 Listener in Variable
+    // B1 + B2
+    View.OnClickListener boLangNghe_XinChao = new View.OnClickListener()
+    {
+        public void onClick (View v)
+        {
+            // Example: hiện thông báo nhanh bằng lớp Toast
+            // Toast.makeText(Ngữ_Cảnh, nội dung_thong_bao, thời gian hiện).show();
+            Toast.makeText(MainActivity.this, "Hello Class", Toast.LENGTH_SHORT).show();
+        }
+    };
+}
