80b935eafc7a36c91d873a9561cfdf8537035437
sophiahua03 <tien.hnt.63cntt@ntu.edu.vn>
2025-03-05 13:06:55 +0700

    update

--- Ex5_AddSubMulDiv_Var/app/src/main/java/Tien63131427/ex5_addsubmuldiv_var/MainActivity.java
+++ Ex5_AddSubMulDiv_Var/app/src/main/java/Tien63131427/ex5_addsubmuldiv_var/MainActivity.java
@@ -1,6 +1,9 @@
 package Tien63131427.ex5_addsubmuldiv_var;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,76 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TimView();
+        nutCong.setOnClickListener(XuLyCong);
+        nutTru.setOnClickListener(XuLyTru);
+        nutNhan.setOnClickListener(XuLyNhan);
+        nutChia.setOnClickListener(XuLyChia);
+
+    }
+
+    View.OnClickListener XuLyCong = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String so1 = editTextSo1.getText().toString();
+            String so2 = editTextSo2.getText().toString();
+            float num1 = Float.parseFloat(so1);
+            float num2 = Float.parseFloat(so2);
+            float tong = num1 + num2;
+            String ChuoiKQ = String.valueOf(tong);
+            editTextKetQua.setText(ChuoiKQ);
+        }
+    };
+    View.OnClickListener XuLyTru = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String so1 = editTextSo1.getText().toString();
+            String so2 = editTextSo2.getText().toString();
+            float num1 = Float.parseFloat(so1);
+            float num2 = Float.parseFloat(so2);
+            float tong = num1 - num2;
+            String ChuoiKQ = String.valueOf(tong);
+            editTextKetQua.setText(ChuoiKQ);
+        }
+    };
+    View.OnClickListener XuLyNhan = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String so1 = editTextSo1.getText().toString();
+            String so2 = editTextSo2.getText().toString();
+            float num1 = Float.parseFloat(so1);
+            float num2 = Float.parseFloat(so2);
+            float tong = num1 * num2;
+            String ChuoiKQ = String.valueOf(tong);
+            editTextKetQua.setText(ChuoiKQ);
+        }
+    };
+    View.OnClickListener XuLyChia = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String so1 = editTextSo1.getText().toString();
+            String so2 = editTextSo2.getText().toString();
+            float num1 = Float.parseFloat(so1);
+            float num2 = Float.parseFloat(so2);
+            float tong = num1 / num2;
+            String ChuoiKQ = String.valueOf(tong);
+            editTextKetQua.setText(ChuoiKQ);
+        }
+    };
+
+    EditText editTextSo1;
+    EditText editTextSo2;
+    EditText editTextKetQua;
+    Button nutCong, nutTru, nutChia, nutNhan;
+    void TimView()
+    {
+        editTextSo1 = (EditText) findViewById(R.id.edtSo1);
+        editTextSo2 = (EditText) findViewById(R.id.edtSo2);
+        editTextKetQua = (EditText) findViewById(R.id.edtKetQua);
+        nutCong = (Button) findViewById(R.id.btnCong);
+        nutTru = (Button) findViewById(R.id.btnTru);
+        nutNhan = (Button) findViewById(R.id.btnNhan);
+        nutChia = (Button) findViewById(R.id.btnChia);
     }
 }
