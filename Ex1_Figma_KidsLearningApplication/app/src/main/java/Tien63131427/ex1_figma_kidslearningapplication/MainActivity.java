0e3fe521ade6ffdabe1e307143aa9111db38c9ae
sophiahua03 <tien.hnt.63cntt@ntu.edu.vn>
2025-03-23 17:29:02 +0700

    update

--- Ex1_Figma_KidsLearningApplication/app/src/main/java/Tien63131427/ex1_figma_kidslearningapplication/MainActivity.java
+++ Ex1_Figma_KidsLearningApplication/app/src/main/java/Tien63131427/ex1_figma_kidslearningapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package Tien63131427.ex1_figma_kidslearningapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnstart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnstart = (Button) findViewById(R.id.tb);
+
+        btnstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentHome = new Intent(MainActivity.this, HomepageActivity.class);
+                startActivity(intentHome);
+            }
         });
     }
 }
