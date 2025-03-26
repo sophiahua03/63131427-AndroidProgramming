978532ec4c1993626c052364abf8c682f0d9dd1f
sophiahua03 <tien.hnt.63cntt@ntu.edu.vn>
2025-03-26 12:26:18 +0700

    update

--- Ex_UsingRecyclerView/app/src/main/java/Tien63131427/ex_usingrecyclerview/MainActivity.java
+++ Ex_UsingRecyclerView/app/src/main/java/Tien63131427/ex_usingrecyclerview/MainActivity.java
@@ -7,8 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recyclerViewDatas;
+    RecyclerView recyclerViewLandscape;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    ArrayList<LandScape> getDataForRecyclerView()
+    {
+        ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
+        LandScape landScape1 = new LandScape();
     }
 }
